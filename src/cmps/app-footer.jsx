447b0ca1c806734641efd2d2152d9c43bab77e5c
Itamar Avady <itamaravady@gmail.com>
2022-02-24 10:33:58 +0200

    gh-pages test

--- src/cmps/app-footer.jsx
+++ src/cmps/app-footer.jsx
@@ -2,13 +2,13 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux'
 
-import { UserMsg } from './user-msg.jsx'
+// import { UserMsg } from './user-msg.jsx'
 
-function _AppFooter({ count }) {
+function _AppFooter() {
     return (
         <footer className="app-footer">
             <p>
-                coffeerights 2021 - count: {count}
+                copyrights 2022 - Itamar Avady
             </p>
         </footer>
     )
